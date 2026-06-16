d20b00cde34cf95d53ad6636d9a00122a57c60ae
demoniotasmania <ubaugianfranco17@gmail.com>
2026-06-16 07:35:43 -0600

    primer commit

--- app/Http/Requests/AudinciaRequest.php
+++ app/Http/Requests/AudinciaRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class case_table extends FormRequest
+class AudinciaRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
